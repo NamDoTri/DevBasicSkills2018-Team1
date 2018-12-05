5eec4d28cbf25a26947395992838fbaf9d171077
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-12-05 13:47:17 +0200

    Added page icon

--- math convert main page/javascript/main.js
+++ math convert main page/javascript/main.js
@@ -1,7 +1,6 @@
 function showClear()
 {
     var buttonID = document.getElementById("showClearButton");
-    console.log(buttonID.textContent);
     if (buttonID.textContent == "Show")
     {
         document.getElementById("numberSystemOutputs").style.visibility = "visible";
