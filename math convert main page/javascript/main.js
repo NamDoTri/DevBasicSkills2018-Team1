dd86447eda4de9b179eff9423922b56974af6803
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-12-07 13:34:02 +0200

    Clear table instead of hiding it

--- math convert main page/javascript/main.js
+++ math convert main page/javascript/main.js
@@ -3,12 +3,12 @@ function showClear()
     var buttonID = document.getElementById("showClearButton");
     if (buttonID.textContent == "Show")
     {
-        document.getElementById("numberSystemOutputs").style.visibility = "visible";
+        printTable();
         buttonID.textContent = "Clear";
     }
     else
     {
-        document.getElementById("numberSystemOutputs").style.visibility = "hidden";
+        document.getElementById("numberSystemOutputs").innerHTML = "";
         buttonID.textContent = "Show";
     }
 }
--- math convert main page/javascript/tableGen.js
+++ math convert main page/javascript/tableGen.js
@@ -1,4 +1,9 @@
-for(var i = 0; i < 51; i++)
+function printTable()
 {
-    document.getElementById("numberSystemOutputs").innerHTML += "<tr> <td>" + i + "</td> <td>"+ fromDec(i,2) +"</td> <td>"+ fromDec(i, 8) +"</td> <td>"+ fromDec(i, 16) +"</td> </tr>";
+    document.getElementById("numberSystemOutputs").innerHTML = "<tr><td>Dec</td><td>Bin</td><td>Oct</td><td>Hex</td></tr>";
+    for(var i = 0; i < 51; i++)
+    {
+        document.getElementById("numberSystemOutputs").innerHTML += "<tr> <td>" + i + "</td> <td>"+ fromDec(i,2) +"</td> <td>"+ fromDec(i, 8) +"</td> <td>"+ fromDec(i, 16) +"</td> </tr>";
+    }
 }
+
