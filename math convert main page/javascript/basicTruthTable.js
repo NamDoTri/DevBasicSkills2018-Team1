f871c6e58a5b8454863cc8a758f2529fdf65facf
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-12-10 23:18:49 +0200

    Shortened basicTruthTable.js file

--- math convert main page/javascript/basicTruthTable.js
+++ math convert main page/javascript/basicTruthTable.js
@@ -1,56 +1,26 @@
-class truthRow{
-    constructor(statement, a, b, c, d)
-    {
-        this.statement = statement;
-        this.first = a;
-        this.second = b;
-        this.third = c;
-        this.forth = d;
-    }
-    showStatement()
-    {
-        return this.statement;
-    }
-    showFirst()
-    {
-        return this.first;
-    }
-    showSecond()
-    {
-        return this.second;
-    }
-    showThird()
-    {
-        return this.third;
-    }
-    showForth()
-    {
-        return this.forth;
-    }
-}
-const arr=[   new truthRow("~P ^ Q", 0, 0, 1, 0),
-              new truthRow( "~P v Q", 0, 1, 1, 0),
-              new truthRow("~P ⊕ Q", 1, 0, 0, 1),
-              new truthRow("~P ⇒ Q", 1, 1, 1, 0) ];
+const arr=[  ["~P ^ Q", 0, 0, 1, 0],
+             ["~P v Q", 0, 1, 1, 0],
+              ["~P ⊕ Q", 1, 0, 0, 1],
+             ["~P ⇒ Q", 1, 1, 1, 0] ];
 for (let a = 0; a < arr.length; a++)
 {
     var truthtable = "";
-    truthtable += "<table><tr><th> P </th>" + "<th> Q </th>" + "<th> ~P </th>" + "<th>" + arr[a].showStatement() + "</th>" + "</tr>";
+    truthtable += "<table><tr><th> P </th>" + "<th> Q </th>" + "<th> ~P </th>" + "<th>" + arr[a][0] + "</th>" + "</tr>";
     for( var i = 0 ; i < 2 ; i++)
     {
         for( var k = 0 ; k < 2 ; k++)
         {
             if( i != 0 && k == 1){
-                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 0 + "</td>" + "<td>" + arr[a].showFirst() + "</td></tr>";
+                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 0 + "</td>" + "<td>" + arr[a][1] + "</td></tr>";
             }
             if(i == 1 && k == 0){
-                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 0 + "</td>" + "<td>" + arr[a].showSecond() + "</td>" + "</tr>";
+                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 0 + "</td>" + "<td>" + arr[a][2] + "</td>" + "</tr>";
             }
             if(i == 0 && k == 1){
-                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 1 + "</td>" + "<td>" + arr[a].showThird() + "</td>" + "</tr>";
+                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 1 + "</td>" + "<td>" + arr[a][3] + "</td>" + "</tr>";
             }
             if(i == 0 && k == 0){
-                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 1 + "</td>" + "<td>" + arr[a].showForth() + "</td>" + "</tr>"; 
+                truthtable = truthtable + "<tr><td>"+ i + "</td>" + "<td>" + k + "</td>" + "<td>"+ 1 + "</td>" + "<td>" + arr[a][4] + "</td>" + "</tr>"; 
             }
         }
     }
