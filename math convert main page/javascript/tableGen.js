f23918f5fa2a1a2eb8d376b518d96914ae2852db
Eetu Heikkinen <eetu.heikkinen12@gmail.com>
2018-12-11 14:48:22 +0200

    Changed dec, bin, hex and oct to be th for styling reasons.

--- math convert main page/javascript/tableGen.js
+++ math convert main page/javascript/tableGen.js
@@ -1,6 +1,6 @@
 function printTable()
 {
-    document.getElementById("numberSystemOutputs").innerHTML = "<tr><td>Dec</td><td>Bin</td><td>Oct</td><td>Hex</td></tr>";
+    document.getElementById("numberSystemOutputs").innerHTML = "<tr><th>Dec</th><th>Bin</th><th>Oct</th><th>Hex</th></tr>";
     for(var i = 0; i < 51; i++)
     {
         document.getElementById("numberSystemOutputs").innerHTML += "<tr> <td>" + i + "</td> <td>"+ fromDec(i,2) +"</td> <td>"+ fromDec(i, 8) +"</td> <td>"+ fromDec(i, 16) +"</td> </tr>";
