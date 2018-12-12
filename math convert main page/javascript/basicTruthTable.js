e76ac3b10a72a3356cae7264436f42d4146b7ff1
Nam Do Tri <t8dotr00@students.oamk.fi>
2018-12-12 17:06:28 +0200

    Added gap between truth tables

--- math convert main page/javascript/basicTruthTable.js
+++ math convert main page/javascript/basicTruthTable.js
@@ -5,7 +5,7 @@ const arr=[  ["~P ^ Q", 0, 0, 1, 0],
 for (let a = 0; a < arr.length; a++)
 {
     var truthtable = "";
-    truthtable += "<table><tr><th> P </th>" + "<th> Q </th>" + "<th> ~P </th>" + "<th>" + arr[a][0] + "</th>" + "</tr>";
+    truthtable += "<table class='truthtruth'><tr><th> P </th>" + "<th> Q </th>" + "<th> ~P </th>" + "<th>" + arr[a][0] + "</th>" + "</tr>";
     for( var i = 0 ; i < 2 ; i++)
     {
         for( var k = 0 ; k < 2 ; k++)
@@ -25,5 +25,6 @@ for (let a = 0; a < arr.length; a++)
         }
     }
     truthtable += "</table><br>";
-    document.getElementById("truthtable").innerHTML += truthtable + "<br></br>";
+    document.getElementById("truthtable").innerHTML += truthtable + "<br>&nbsp<br>";
 }
+
